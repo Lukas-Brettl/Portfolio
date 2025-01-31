874ad0306909d9cd683035b1d27b3107eace3964
brettl <lukas.brettl@seznam.cz>
2025-01-31 16:01:47 +0100

    17. trying to fix upload to github pages

--- src/main.jsx
+++ src/main.jsx
@@ -15,10 +15,10 @@ root.render(
   <BrowserRouter basename="/Portfolio"> {/* NE import.meta.env.BASE_URL */}
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/technologies" element={<Technologies />} />
-      <Route path="/searchCountry" element={<ResearchCountry />} />
-      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/Portfolio/projects" element={<Projects />} />
+      <Route path="/Portfolio/technologies" element={<Technologies />} />
+      <Route path="/Portfolio/searchCountry" element={<ResearchCountry />} />
+      <Route path="/Portfolio/calculator" element={<Calculator />} />
     </Routes>
   </BrowserRouter>
 );
