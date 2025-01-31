8f94d37de348362037880e3ec04d57c31efa56e4
brettl <lukas.brettl@seznam.cz>
2025-01-31 15:07:56 +0100

    5. trying to fix upload to github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,24 @@
-import { createRoot } from "react-dom/client"; // Import createRoot from react-dom/client
-import { BrowserRouter, Routes, Route } from "react-router-dom"; // Import Routes and Route
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './index.css'
-import Home from './Home.jsx'
-import Projects from "./Projects.jsx";
-import Technologies from "./Technologies.jsx"
-import Calculator from "./try/Calculator.jsx";
-import ResearchCountry from "./try/ResearchCountry.jsx"
+import './index.css';
+import Home from './Home.js';
+import Projects from "./Projects.js";
+import Technologies from "./Technologies.js";
+import Calculator from "./try/Calculator.js";
+import ResearchCountry from "./try/ResearchCountry.js";
 
-createRoot(document.getElementById('root'), {
-  future: {
-    v7_startTransition: true,
-  },
-}).render(
-  <BrowserRouter basename="/Portfolio" future={{
-    v7_relativeSplatPath: true,
-  }}>
-      <Routes>
-        <Route path="projects" element={<Projects/>}/>
-        <Route path="technologies" element={<Technologies/>}/>
-        <Route path="searchCountry" element={<ResearchCountry/>}/>
-        <Route path="calculator" element={<Calculator/>}/>
-        <Route index element={<Home/>}/>
-      </Routes>
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/technologies" element={<Technologies />} />
+      <Route path="/searchCountry" element={<ResearchCountry />} />
+      <Route path="/calculator" element={<Calculator />} />
+    </Routes>
   </BrowserRouter>
-)
+);
